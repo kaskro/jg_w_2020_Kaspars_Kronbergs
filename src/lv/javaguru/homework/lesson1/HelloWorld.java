b73970c6d25c2f2ce22883f47b64d44ab4c19125
Kaspars <kaspars.kronbergs@gmail.com>
2020-01-29 21:34:01 +0200

    Added random comment to test push.

--- src/lv/javaguru/homework/lesson1/HelloWorld.java
+++ src/lv/javaguru/homework/lesson1/HelloWorld.java
@@ -2,6 +2,7 @@ package lv.javaguru.homework.lesson1;
 
 public class HelloWorld {
     public static void main(String[] args){
+//        Added this comment for commit
         System.out.println("Hello world!");
     }
 
