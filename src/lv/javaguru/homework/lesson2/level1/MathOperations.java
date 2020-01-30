2c8e919031c79d91f895d88d2a7768ead18d0437
Kaspars <kaspars.kronbergs@gmail.com>
2020-01-30 15:17:41 +0200

    Lesson2 homework.

---
+++ src/lv/javaguru/homework/lesson2/level1/MathOperations.java
@@ -0,0 +1,14 @@
+package lv.javaguru.homework.lesson2.level1;
+
+public class MathOperations {
+    public static void main(String[] args) {
+
+        int firstNumber = 10;
+        int secondNumber = 5;
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+        System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+        System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+        System.out.println(firstNumber + " / " + secondNumber + " = " + (firstNumber / secondNumber));
+    }
+}
---
+++ src/lv/javaguru/homework/lesson2/level2/RandomNumbersServiceTest.java
@@ -0,0 +1,19 @@
+package lv.javaguru.homework.lesson2.level2;
+
+import java.util.Random;
+
+public class RandomNumbersServiceTest {
+    public static void main(String[] args) {
+
+        Random randomGenerator = new Random();
+
+        int firstRandomNumber = randomGenerator.nextInt(100);
+        int secondRandomNumber = randomGenerator.nextInt(100);
+        int thirdRandomNumber = randomGenerator.nextInt(100);
+
+        System.out.print(firstRandomNumber + " + ");
+        System.out.print(secondRandomNumber + " + ");
+        System.out.print(thirdRandomNumber + " = ");
+        System.out.println(firstRandomNumber + secondRandomNumber + thirdRandomNumber);
+    }
+}
